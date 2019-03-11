92e2a8dc18d9d471a20abd034545efd2881f06a9
Mainak <MS@MS-PC>
2019-03-11 08:15:16 +0530

    Changes to fit the war to deploy external server

--- src/main/java/com/easybusiness/hrmanagement/HRManagmentapplicationApplication.java
+++ src/main/java/com/easybusiness/hrmanagement/HRManagmentapplicationApplication.java
@@ -2,12 +2,17 @@ package com.easybusiness.hrmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@ComponentScan("com.easybusiness.usermanagement.entity")
-public class HRManagmentapplicationApplication {
+public class HRManagmentapplicationApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HRManagmentapplicationApplication.class);
+	}
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(HRManagmentapplicationApplication.class, args);
 	}
