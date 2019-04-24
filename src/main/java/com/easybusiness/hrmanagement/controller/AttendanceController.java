75bda90193e1af0b99c3d788ac5c8563f7ede81f
sandip <sandip@sandip-PC>
2019-04-24 22:52:36 +0530

    Attendance

--- src/main/java/com/easybusiness/hrmanagement/controller/AttendanceController.java
+++ src/main/java/com/easybusiness/hrmanagement/controller/AttendanceController.java
@@ -1,8 +1,6 @@
 package com.easybusiness.hrmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +14,8 @@ public class AttendanceController {
 	@Autowired
 	AttendanceDetailsService attendanceDetailsService;
 	
-	@GetMapping("/findByFileIdEmpId/file/{fileId}/emp/{empId}")
-	public void getAttendanceDetailsByFileIdEmpId(@PathVariable("fileId") Long fileId, @PathVariable("empId") Long empId) throws Exception {
+	private void getAttendanceDetailsByFileIdEmpId(Long fileId, Long empId) throws Exception {
+		
 		attendanceDetailsService.findByFileIdEmpId(fileId, empId);
 		
 	}
