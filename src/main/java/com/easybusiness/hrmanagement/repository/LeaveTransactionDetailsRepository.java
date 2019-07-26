a0bd9943e96ca60399c7ce1bbdfb818ca31c7468
Sandip <Sandip@Sandip-HP>
2019-07-26 20:28:47 +0530

    Leave details based on Date Range POC

--- src/main/java/com/easybusiness/hrmanagement/repository/LeaveTransactionDetailsRepository.java
+++ src/main/java/com/easybusiness/hrmanagement/repository/LeaveTransactionDetailsRepository.java
@@ -18,5 +18,11 @@ public interface LeaveTransactionDetailsRepository extends CrudRepository<LeaveT
 	@Query("select t from LeaveTransactionDetails t where t.userId =:userId and t.leaveStartDate <= :startDate and t.leaveEndDate >=:endDate")
 	public List<LeaveTransactionDetails> getLeaveTransactionDetailsByuserIdDateRange(@Param("userId") Long userId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
-	
+	/*
+	 * 15-07-19 to 22-07-19 SELECT * FROM LEAVE_TRANSACTION_DETAILS WHERE
+	 * USER_ID=100 AND ((LEAVE_START_DATE <= '15-07-19' and LEAVE_END_DATE
+	 * >='22-07-19') OR (LEAVE_START_DATE >= '15-07-19' and LEAVE_END_DATE
+	 * <='22-07-19') OR (LEAVE_START_DATE <= '22-07-19' and LEAVE_END_DATE
+	 * >='15-07-19') );
+	 */
 }
