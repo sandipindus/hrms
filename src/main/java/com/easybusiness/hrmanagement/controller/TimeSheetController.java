0c565430a97670e79a926eb9e136ebb1c723164a
sandip <sandip@sandip-PC>
2019-08-05 22:29:35 +0530

    setModifiedDate

--- src/main/java/com/easybusiness/hrmanagement/controller/TimeSheetController.java
+++ src/main/java/com/easybusiness/hrmanagement/controller/TimeSheetController.java
@@ -81,69 +81,13 @@ public class TimeSheetController {
 	public ReturnMessage updateTimesheetApproval(@RequestBody List<Timesheet> timesheetList) throws Exception {
 		
 		for (Timesheet eachTimesheet : timesheetList) {
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			eachTimesheet.setModifiedDate(timestamp);
 			timesheetService.updateTimesheet(eachTimesheet);
 		}
 		
 		ReturnMessage msg = new ReturnMessage("Timesheet " + HRManagementConstant.UPDATED_SUCCESSFULLY);
 		return msg;
-	}
-	
-	@GetMapping("/f")
-	public List<Timesheet> getTimesheetFirst() {
-		List<Timesheet> list = new ArrayList<>();
-		
-		Timesheet ts = new Timesheet();
-		ts.setId(new Long(1));
-		ts.setEmpId(new Long(100));
-		ts.setProjectNumber(new Long(1));
-		ts.setProjectTypeID(new Long(2));
-		ts.setTaskId(new Long(2));
-		ts.setYear(new Long(19));
-		ts.setDayRange("JUNE-10-14");
-		ts.setHourMon(new Long(8));
-		ts.setHourTue(new Long(8));
-		ts.setApproverId(new Long(200));
-		ts.setApproveStatus(new Long(0));
-		ts.setCreatedBy(new Long(100));
-		ts.setModifiedBy(new Long(100));
-		ts.setIsLeaveHour(new Long(0));
-		ts.setBillable("Billable");
-		
-		Timesheet ts1 = new Timesheet();
-		ts1.setId(new Long(2));
-		ts1.setEmpId(new Long(100));
-		ts1.setProjectNumber(new Long(1));
-		ts1.setProjectTypeID(new Long(3));
-		ts1.setTaskId(new Long(3));
-		ts1.setYear(new Long(19));
-		ts1.setDayRange("JUNE-10-14");
-		ts1.setHourWed(new Long(8));
-		ts1.setHourThu(new Long(8));
-		ts1.setHourFri(new Long(8));
-		ts1.setApproverId(new Long(200));
-		ts1.setApproveStatus(new Long(0));
-		ts1.setCreatedBy(new Long(100));
-		ts1.setModifiedBy(new Long(100));
-		ts1.setIsLeaveHour(new Long(0));
-		ts1.setBillable("Billable");
-		
-		Timesheet ts2 = new Timesheet();
-		ts2.setId(new Long(3));
-		ts2.setEmpId(new Long(100));
-		ts2.setYear(new Long(19));
-		ts2.setDayRange("JUNE-10-14");
-		ts2.setHourSat(new Long(8));
-		ts2.setApproverId(new Long(200));
-		ts2.setApproveStatus(new Long(0));
-		ts2.setCreatedBy(new Long(100));
-		ts2.setModifiedBy(new Long(100));
-		ts2.setIsLeaveHour(new Long(1));
-		ts2.setBillable("Billable");
-		
-		list.add(ts);
-		list.add(ts1);
-		list.add(ts2);
-		return list;
 	}
 	
 	private void validateTimeSheet(Timesheet timesheet) throws Exception {
