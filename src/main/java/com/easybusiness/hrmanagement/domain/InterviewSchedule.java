6135e4c62863353f825d614bd3ac7396c2b23c04
Sandip <Sandip@Sandip-HP>
2019-11-03 16:18:25 +0530

    interview schedule change

--- src/main/java/com/easybusiness/hrmanagement/domain/InterviewSchedule.java
+++ src/main/java/com/easybusiness/hrmanagement/domain/InterviewSchedule.java
@@ -7,6 +7,8 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -22,6 +24,7 @@ public class InterviewSchedule implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id()
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "INTRVW_SCHDL_ID")
     private Long intrSchdlID;
     
